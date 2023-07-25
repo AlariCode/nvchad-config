5349be3afc966ed774ee4609f2bdbc5f83e02100
AlariCode <antonlarichev@gmail.com>
2023-07-25 21:56:41 +0300

    новые плагины

--- plugins.lua
+++ plugins.lua
@@ -163,7 +163,20 @@ local plugins = {
       require("todo-comments").setup()
     end,
   }, -- To make a plugin not be loaded
-  -- {
+  {
+    "topaxi/gh-actions.nvim",
+    cmd = "GhActions",
+    keys = {
+      { "<leader>gh", "<cmd>GhActions<cr>", desc = "Open Github Actions" },
+    },
+    -- optional, you can also install and use `yq` instead.
+    build = "make",
+    dependencies = { "nvim-lua/plenary.nvim", "MunifTanjim/nui.nvim" },
+    opts = {},
+    config = function(_, opts)
+      require("gh-actions").setup(opts)
+    end,
+  }, -- {
   --   "NvChad/nvim-colorizer.lua",
   --   enabled = false
   -- },
